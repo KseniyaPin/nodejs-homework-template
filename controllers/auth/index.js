41e90a5d2e44fd365f091f8add1b656b95eb0ae6
KseniyaPin <kontr.mat@gmail.com>
2023-10-27 10:39:52 +0200

    export avatar

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -2,26 +2,10 @@ const getCurrent = require("./getCurrent");
 const login = require("./login");
 const logout = require("./logout");
 const register = require("./register");
+const updateAvatar = require("./updateAvatar");
 
 const {  ctrlWrapper } = require("../../helpers");
 
-const path = require("path");
-const fs = require("fs/promises");
-const { User } = require("../../models/user");
-const avatarsDir = path.join(__dirname, "../", "public", "avatars");
-const updateAvatar = async (req, res) => {
-const avatarsDir = path.join(__dirname, "../", "public", "avatars");
-	const { _id } = req.user;
-	const { path: tempUpload, originalname } = req.file;
-	const resultUpload = path.join(avatarsDir, originalname);
-	await fs.rename(tempUpload, resultUpload);
-	const avatarURL = path.join("avatars", originalname);
-	await User.findByIdAndUpdate(_id, {avatarURL});
-
-	res.json({
-		avatarURL,
-	})
-}
 
 module.exports = {
 	register: ctrlWrapper(register),
