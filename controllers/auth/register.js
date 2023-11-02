c566c0cd3b5e7e714b57aeebc9b59bbc05ccc0db
KseniyaPin <kontr.mat@gmail.com>
2023-11-02 22:34:11 +0100

    Update register.js

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const gravatar = require("gravatar");
-const {nanoid} = require(nanoid);
+const {nanoid} = require("nanoid");
 
 const { User } = require("../../models/user");
 
