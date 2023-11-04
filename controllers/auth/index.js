b0a0e33e620695f1b5b3bc5eaa229bcbbebabee9
KseniyaPin <kontr.mat@gmail.com>
2023-11-04 23:03:55 +0100

    url

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,6 +3,8 @@ const login = require("./login");
 const logout = require("./logout");
 const register = require("./register");
 const updateAvatar = require("./updateAvatar");
+const verifyEmail = require("./verifyEmail");
+const resendVerifyEmail = require("./resendVerifyEmail");
 
 const {  ctrlWrapper } = require("../../helpers");
 
@@ -14,4 +16,6 @@ module.exports = {
 	getCurrent: ctrlWrapper(getCurrent),
 	logout: ctrlWrapper(logout),
 	updateAvatar: ctrlWrapper(updateAvatar),
+	verifyEmail: ctrlWrapper(verifyEmail),
+	resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 }
--- models/user.js
+++ models/user.js
@@ -54,6 +54,10 @@ const registerSchema = Joi.object({
 	password: Joi.string().min(6).required(),
 });
 
+const emailSchema = Joi.object({
+	email: Joi.string().pattern(emailRegexp).required(),
+});
+
 const loginSchema = Joi.object({
 	email: Joi.string().pattern(emailRegexp).required(),
 	password: Joi.string().min(6).required(),
@@ -62,6 +66,7 @@ const loginSchema = Joi.object({
 const schemas = {
 	registerSchema,
 	loginSchema,
+	emailSchema,
 };
 
 const User = model("user", userSchema);
